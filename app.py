9002925a6dda1c70cfd28ba57584c405529daa34
master_chef_2811 <assassinnyt209@gmail.com>
2023-05-05 20:34:13 +0700

    Add route form add data

--- app.py
+++ app.py
@@ -49,6 +49,11 @@ def login_required(func): # need for all router
         return func(*args, **kwargs)
     return secure_function
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
 @app.route("/")
 @app.route("/login", methods=['GET','POST'])
 def login():
@@ -78,7 +83,8 @@ def login():
                                    pass_check='False')
     
         my_user = user_data[0]
-        
+        session['username'] = my_user
+         
         cur.execute("""
                 SELECT r.role_folder
                 FROM role r
@@ -138,7 +144,7 @@ def form_add_data_group_info():
         if tfidf_file.filename != '':
             if tfidf_file.filename.split(".")[-1] != 'pickle':
                 return "Error"
-            pathToFile = app.config['TFIDF_FOLDER'] + "/tfidf_" + str(id_max) + ".pickle"
+            pathToFile = app.config['TFIDF_FOLDER'] + "tfidf_" + str(id_max) + ".pickle"
             tfidf_file.save(pathToFile)
             
         sql = """
@@ -261,7 +267,72 @@ def form_add_account():
 
 @app.route("/form_add_data_train", methods=['GET','POST'])
 def form_add_data_train():
+    cur = mysql.connection.cursor()
     
+    if request.method == 'POST':
+        if 'FileDataTrainUpload' not in request.files.keys():
+            return "Error 1"
+        else:
+            data_file = request.files['FileDataTrainUpload']
+            
+        # file processing
+        if data_file.filename == '':
+            return "Error"
+        
+        if data_file.filename.split(".")[-1] != 'csv':
+            return "Error"
+        
+        data = pd.read_csv(data_file, index_col=0, delimiter="|")
+        
+        # template sql 
+        sql_data_train_template = "INSERT INTO data_train(text, class_id, create_by, update_by) VALUES (%s, %s, %s, %s)"
+        sql_select_data_train_id = "SELECT id_dtrain FROM data_train WHERE text = %s"
+        # insert group
+        sql_check_group = "SELECT * FROM data_group_split WHERE id_dtrain = %s AND id_dgroup = %s"
+        sql_insert_group = "INSERT INTO data_group_split(id_dtrain, id_dgroup) VALUES (%s, %s)"
+        
+        id_exists = list(set(",".join(list(data['group_ids'].values)).split(",")))
+        for elm in id_exists:
+            # check group_id exist
+            cur.execute("""
+                        SELECT *
+                        FROM data_group_info
+                        WHERE id_dgroup = %s
+                        """, (int(elm), ))
+            check_group_exist = cur.fetchall()
+            if len(check_group_exist) == 0:
+                id_exists.remove(elm)
+
+        for i in range(data.shape[0]):
+            label = data['Target'][i]
+            text = data['corpus'][i]
+            group_ids = data['group_ids'][i]
+            
+            # check if text is in database
+            # if exist -> insert + take id
+            # else -> take id
+            cur.execute(sql_select_data_train_id, (text, ))
+            data_id = cur.fetchall()
+            if len(data_id) == 0:
+                cur.execute(sql_data_train_template, (text, label,
+                                                      session['username'][1],
+                                                      session['username'][1]))
+                mysql.connection.commit()
+                cur.execute(sql_select_data_train_id, (text, ))
+                data_id = cur.fetchall()
+            data_id = data_id[0][0]
+            
+            for id_group in group_ids.split(","):
+                
+                if id_group not in id_exists:
+                    continue
+                
+                cur.execute(sql_check_group, (data_id, int(id_group)))
+                check_group_exist = cur.fetchall()
+                if len(check_group_exist) == 0:
+                    cur.execute(sql_insert_group, (data_id, int(id_group)))
+                    mysql.connection.commit()
+        return redirect(url_for('form_add_data_train'))
     return render_template(session['role'] + "/form_add_data_train.html")
 
 @app.route("/change_to_data_train/<string:id_data_input>", methods=['GET','POST'])
