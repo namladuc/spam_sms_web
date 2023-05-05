b5329e28b64cb05af906287a3dd16e1cc6a6ce35
neiknguyen040402 <kiennguyengtglhd@gmail.com>
2023-05-05 23:43:49 +0700

    commit add_accout,register,view dtrain,view dgroup, view acc

--- app.py
+++ app.py
@@ -109,7 +109,38 @@ def login():
 
 @app.route("/register_account", methods=['GET','POST'])
 def register_account():
-    return render_template("general/register_account.html")
+    if request.method == 'POST':
+        details = request.form
+        user_name = details['username'].strip()
+        full_name = details['fullname'].strip()
+        role_id = 2
+        password = details['password'].strip()
+        confirm_password = details['confirm_password'].strip()
+
+        if password != confirm_password:
+            error = "Mật khẩu xác nhận không khớp!"
+            return render_template('general/register_account.html', error=error)
+
+        hashed_password = hashlib.md5(password.lower().encode()).hexdigest()
+
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO user (full_name, username, password) VALUES (%s, %s, %s)",
+                    (full_name, user_name, hashed_password))
+
+        user_id = cur.lastrowid  # Lấy id của user vừa thêm
+
+        cur.execute("INSERT INTO role_user (id_role, id_user) VALUES (%s, %s)",
+                    (role_id, user_id))
+
+        mysql.connection.commit()
+        cur.close()
+
+        message = "Tài khoản đã được tạo thành công!"
+        return render_template('general/register_account.html', message=message)
+
+    return render_template('general/register_account.html')
+
 
 @app.route("/home", methods=['GET','POST'])
 def home():
@@ -223,7 +254,17 @@ def form_add_model():
 @app.route("/view_data_train", methods=['GET','POST'])
 def view_data_train():
     # View du lieu trong bang data train
-    return None
+    cur = mysql.connection.cursor()
+    sql = """
+                SELECT *
+                FROM data_train 
+                join class on data_train.class_id = class.class_id
+            """
+    cur.execute(sql)
+    dtrain = cur.fetchall()
+
+    return render_template(session['role'] + "/view_data_train.html", data=dtrain)
+
 
 @app.route("/view_data_group_info")
 def view_data_group_info():
@@ -267,11 +308,57 @@ def check_data_input():
 
 @app.route("/view_account", methods=['GET','POST'])
 def view_account():
-    return None
+    cur = mysql.connection.cursor()
+    sql = """
+                SELECT * FROM user
+                join role_user on user.id = role_user.id_user
+                join role on role.role_id = role_user.id_role
+          """
+    cur.execute(sql)
+    accounts = cur.fetchall()
+
+    return render_template(session['role'] + "/view_account.html", data=accounts)
 
 @app.route("/form_add_account", methods=['GET','POST'])
 def form_add_account():
-    return None
+    cur = mysql.connection.cursor()
+    sql = """
+            SELECT *
+            FROM role
+          """
+    cur.execute(sql)
+    list_role = cur.fetchall()
+
+    if request.method == 'POST':
+        details = request.form
+        user_name = details['username'].strip()
+        full_name = details['fullname'].strip()
+        role_id = details['role'].strip()
+        password = details['password'].strip()
+        confirm_password = details['confirm_password'].strip()
+
+        if password != confirm_password:
+            error = "Mật khẩu xác nhận không khớp!"
+            return render_template(session['role'] + '/form_add_account.html', error=error,  list_role=list_role)
+
+        hashed_password = hashlib.md5(password.lower().encode()).hexdigest()
+
+        cur.execute("INSERT INTO user (full_name, username, password) VALUES (%s, %s, %s)",
+                    (full_name, user_name, hashed_password))
+
+        user_id = cur.lastrowid   # Lấy id của user vừa thêm
+
+        cur.execute("INSERT INTO role_user (id_role, id_user) VALUES (%s, %s)",
+                    (role_id, user_id))
+
+        mysql.connection.commit()
+        cur.close()
+
+        message = "Tài khoản đã được tạo thành công!"
+        return render_template(session['role'] + '/form_add_account.html', message=message,  list_role=list_role)
+
+    return render_template(session['role'] + '/form_add_account.html', list_role=list_role)
+
 
 @app.route("/form_add_data_train", methods=['GET','POST'])
 def form_add_data_train():
